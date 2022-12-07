fb951f81cb2c0dd1e2f144e08c835778030be4a2
vilmachristensen <83406266+vilmachristensen@users.noreply.github.com>
2022-12-07 09:57:11 +0100

    SkapaKonto-actionmetod tillagd, funktioner i ProfilMetod tillagd

--- Project_Databas/Controllers/ProfilController.cs
+++ Project_Databas/Controllers/ProfilController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Project_Databas.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,6 +16,42 @@ namespace Project_Databas.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Inloggning()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SkapaKonto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SkapaKonto(ProfilDetaljer pd)
+        {
+            ProfilMetod pm = new ProfilMetod();
+            int i = 0;
+            string error = "";
+            i = pm.SkapaKonto(pd, out error);
+            ViewBag.error = error;
+            ViewBag.antal = i;
+
+            if (i == 1) { return RedirectToAction("Index"); }
+            else return View("SkapaKonto");
+        }
+
+        /*
+         * TODO: Denna
+        [HttpPost]
+        public IActionResult Inloggning(ProfilDetaljer pd)
+        {
+         
+        }
+        */
+        
     }
 }
 
--- Project_Databas/Models/ProfilMetod.cs
+++ Project_Databas/Models/ProfilMetod.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -8,13 +10,60 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Project_Databas.Models
 {
-    public class ProfilMetod : Controller
+    public class ProfilMetod
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        public ProfilMetod()
         {
-            return View();
         }
+
+        public IConfigurationRoot GetConnection()
+
+        {
+
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appSettings.json").Build();
+
+            return builder;
+
+        }
+
+        public int SkapaKonto(ProfilDetaljer pd, out string errormsg)
+        {
+            //Skapa SqlConnection
+            SqlConnection dbConnection = new SqlConnection(GetConnection().GetSection("ConnectionStrings").GetSection("DefaultConnection").Value);
+
+
+            //SqlString och lägg till en user i databasen
+            String sqlstring = "INSERT INTO [Tbl_Profil]([Pr_Namn], [Pr_Mail], [Pr_Bor], [Pr_Losenord]) VALUES (@Pr_Namn, @Pr_Mail, @Pr_Bor, @Pr_Losenord)";
+            SqlCommand dbCommand = new SqlCommand(sqlstring, dbConnection);
+
+            dbCommand.Parameters.Add("Pr_Namn", SqlDbType.NVarChar, 30).Value = pd.Pr_Namn;
+            dbCommand.Parameters.Add("Pr_Mail", SqlDbType.NVarChar, 30).Value = pd.Pr_Mail;
+            dbCommand.Parameters.Add("Pr_Bor", SqlDbType.Int).Value = pd.Pr_Bor;
+            dbCommand.Parameters.Add("Pr_Losenord", SqlDbType.Int).Value = pd.Pr_Losenord;
+
+
+            try
+            {
+                //Öppna connection till databasen
+                dbConnection.Open();
+                int i = 0;
+                //Skicka SQL-frågan till databasen, får tillbaka en int
+                i = dbCommand.ExecuteNonQuery();
+                if (i == 1) { errormsg = ""; }
+                else { errormsg = "Det skapas inte en person i databasen."; }
+                return (i);
+            }
+            catch (Exception e)
+            {
+                errormsg = e.Message;
+                return 0;
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+        }
+
     }
 }
 
