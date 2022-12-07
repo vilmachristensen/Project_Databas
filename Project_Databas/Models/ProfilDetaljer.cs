52a499288732ac3423d3d54d907ae90a4ca74137
id19lns <id19lns@cs.umu.se>
2022-12-07 09:24:32 +0100

    Lagt till persondetaljer

--- Project_Databas/Models/ProfilDetaljer.cs
+++ Project_Databas/Models/ProfilDetaljer.cs
@@ -1,20 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Project_Databas.Models
 {
-    public class ProfilDetaljer : Controller
+    public class ProfilDetaljer
     {
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public ProfilDetaljer() { }
+
+        [Display(Name = "Id")]
+        public int Pr_Id { get; set; }
+
+        [Display(Name = "För och efternamn")]
+        public string Pr_Namn { get; set; }
+
+        [Display(Name = "Lösenord")]
+        public string Pr_Losenord { get; set; }
+
+        [Display(Name = "Mailadress")]
+        public string Pr_Mail { get; set; }
+
+        [Display(Name = "Län")]
+        public int Pr_Bor { get; set; }
     }
+
 }
+
+
+
+ 
 
